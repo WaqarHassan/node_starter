ae010028dc5e1387267c96411681b604ef04a3a5
WaqarHassan <whassan25@gmail.com>
2018-09-23 12:51:18 +0500

    google oauth stretegy for express

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const app = express();
-app.get("/" , (req, res) => {
-    res.send({hi: "there"})
-});
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-//Dynamic Port binding
+const app = express();
+
+//client id = 298169812039-if5a3bpleebgm3k9pu32ovg2drpsoam7.apps.googleusercontent.com
+//client secret = EsfvVsnVBvsMT9sypQuJrIBI
+passport.user(new GoogleStrategy( ));
+//Dynamic Port 	binding
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT);
