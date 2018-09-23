66d3e88830d6a940bc7ef40c916416813142e81c
WaqarHassan <whassan25@gmail.com>
2018-09-23 22:39:44 +0500

    google oauth callback

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
+
 
 const app = express();
 
-//client id = 298169812039-if5a3bpleebgm3k9pu32ovg2drpsoam7.apps.googleusercontent.com
-//client secret = EsfvVsnVBvsMT9sypQuJrIBI
-passport.user(new GoogleStrategy( ));
+passport.use(new GoogleStrategy( {
+	clientID: keys.googleClientID,
+	clientSecret: keys.googleClientSecret,
+	callbackURL: '/auth/google/callback'
+}, accessToken => {
+	console.log(accessToken)
+}));
+
+app.get(
+	'/auth/google' ,
+	 passport.authenticate('google', {
+		scope: ['profile', 'email']
+	}));
+app.get('auth/google/callback',  passport.authenticate('google'));   
 //Dynamic Port 	binding
 const PORT = process.env.PORT || 5000
 
