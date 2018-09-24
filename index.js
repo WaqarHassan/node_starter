af6365a97b2d21b0d3949f365e125267e71a36b5
WaqarHassan <whassan25@gmail.com>
2018-09-24 22:26:30 +0500

    routes services server refactoring

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys')
-
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+  
+  authRoutes(app); // require('./routes/authRoutes')(app);
 
-passport.use(new GoogleStrategy( {
-	clientID: keys.googleClientID,
-	clientSecret: keys.googleClientSecret,
-	callbackURL: '/auth/google/callback'
-}, (accessToken, refereshToken, profile, done) => {
-	console.log('accessToken '+accessToken)
-	console.log('refereshToken '+refereshToken)
-	console.log('profile '+ JSON.stringify(profile))
-
-}));
-
-app.get(
-	'/auth/google' ,
-	 passport.authenticate('google', {
-		scope: ['profile', 'email']
-	}));
-app.get('/auth/google/callback',
-  passport.authenticate('google'));    
 //Dynamic Port 	binding
 const PORT = process.env.PORT || 5000
 
